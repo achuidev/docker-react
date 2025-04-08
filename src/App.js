17cd9815a36a6ce992b8b46873765eb0a5e0dfd4
Akshay Chandwani <akshaychandwani@Akshays-MacBook-Pro.local>
2025-04-07 20:29:15 -0400

    Changed Text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi Akshay!.
+          Hi Akshay! This is a AWS Deployed application Wohoooooo!
         </p>
         <a
           className="App-link"
